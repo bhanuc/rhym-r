29c6983c370977e4b7020667d51daeb53f9bfb27
bhanuc <bhanuc@iitk.ac.in>
2014-11-23 03:40:04 +0530

    updated mail query

---
+++ js/gun.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+		
+	$("#leadForm").on("submit", function(e) {
+		e.preventDefault();
+
+		console.log("Handling the submit");
+		//add error handling here
+		//gather the form data
+		var data = {};
+		data.name = $("#name").val();
+		data.email = $("#email").val();
+		data.area = $("#mobno").val();
+		data.query = $("#query").val();
+	});
+	
+});
+
+/* global $,document,console,Parse */
+$(document).ready(function() {
+	
+	var parseAPPID = "btW0MTLsNYLvikmhYt3U4fWOXzKl9SgE30AXh8E9";
+	var parseJSID = "A5niEfk6ddKZxXNPmj4JGDBXK6gNyzK5V6vEX8UV";
+	
+	Parse.initialize(parseAPPID, parseJSID);
+	var CommentObject = Parse.Object.extend("CommentObject");
+	
+	$("#commentForm").on("submit", function(e) {
+		e.preventDefault();
+
+		console.log("Handling the submit");
+		//add error handling here
+		//gather the form data
+
+		var data = {};
+		data.name = $("#name").val();
+		data.email = $("#email").val();
+		data.area = $("#mobno").val();
+		data.query = $("#query").val();
+
+		var comment = new CommentObject();
+		comment.save(data, {
+			success:function() {
+				console.log("Success");
+				//Alerts are lame - but quick and easy
+				alert("Thanks for filling the form!");
+			},
+			error:function(e) {
+				console.dir(e);
+			}
+		});
+		
+	});
+	
+});
---
+++ mailgun/cloud/main.js
@@ -0,0 +1,34 @@
+
+
+var Mailgun = require('mailgun');
+Mailgun.initialize('site.salarpuria-rhythm.com', 'key-b046bedeefacfebc624803b2f32a4504');
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", function(request, response) {
+  response.success("Hello world!");
+});
+
+Parse.Cloud.beforeSave("CommentObject", function(request, response) {
+
+	var text = "Query Email\n" + 
+		"From: "+request.object.get("name") + "\n"+
+		"Email: "+request.object.get("email") + "\n"+
+		"Mobile No: "+request.object.get("mobno") + "\n\n"+
+		"Query:\n" + request.object.get("query");
+	
+	Mailgun.sendEmail({
+			to: "bhanu423@gmail.com",
+			from: request.object.get("email"),
+			subject: "Query Form  ",
+			text: text
+		}, {
+		success: function(httpResponse) {
+			response.success();
+		},
+		error: function(httpResponse) {
+			console.error(httpResponse);
+			response.error("Uh oh, something went wrong");
+		}
+	});
+
+});
